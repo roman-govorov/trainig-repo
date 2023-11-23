666be481f809a5a65c67f688ce0e8d9ba0b008b2
sonicfon <58296644+sonicfon@users.noreply.github.com>
2023-11-23 23:45:35 +0700

    Наработка для кода

--- tests/Division/Division.js
+++ tests/Division/Division.js
@@ -1,6 +1,13 @@
+/*function divide(Numb1,Numb2){
+    try {
+        
+    } catch (error) {
+        
+    }
+}*/
 let Numb1 = Number;
 let Numb2 = Number;
-let resultat = Number;
+let resultat = Float32Array;
 if (Numb2 != 0){
     resultat = Numb1 / Numb2;
 }
