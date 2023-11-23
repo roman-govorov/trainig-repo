ba29e92270cc567e4704a5219a6b7e727e44135e
sonicfon <58296644+sonicfon@users.noreply.github.com>
2023-11-23 21:58:57 +0700

    Модуль деления (без юнит тестов)

---
+++ Division.js
@@ -0,0 +1,7 @@
+let Numb1 = Number(prompt("введите первое число", ""));
+let Numb2 = Number(prompt("введите второе число", ""));
+let resultat = Number;
+if (Numb2 != 0){
+    resultat = Numb1 / Numb2;
+}
+alert(resultat);
