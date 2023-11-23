c427fdb9b3f67f718bbcfa00f43e7f438d8e718a
FaVecNer <151643268+FaVecNer@users.noreply.github.com>
2023-11-24 00:51:49 +0700

    second

--- Exponentation.script/TestUnit 1-5/exponentation-test3.js
+++ Exponentation.script/TestUnit 1-5/exponentation-test3.js
@@ -1,3 +1,10 @@
 function exponentation(n1, n2) {
     return Math.pow(n1, n2);
 }
+let otv = exponentation(-2, 3)
+if (otv === -8) {
+    console.log("Successful test" + otv)
+}
+else {
+    console.error("Test failed" + otv)
+}
