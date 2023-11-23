a31881cc57294b10ae7408da18a50c94a7d80768
sonicfon <58296644+sonicfon@users.noreply.github.com>
2023-11-23 22:40:26 +0700

    Стилизация под модуль

--- tests/Division/Division.js
+++ tests/Division/Division.js
@@ -1,7 +1,7 @@
-let Numb1 = Number(prompt("введите первое число", ""));
-let Numb2 = Number(prompt("введите второе число", ""));
+let Numb1 = Number;
+let Numb2 = Number;
 let resultat = Number;
 if (Numb2 != 0){
     resultat = Numb1 / Numb2;
 }
-alert(resultat);
+export {Numb1,Numb2,resultat}
