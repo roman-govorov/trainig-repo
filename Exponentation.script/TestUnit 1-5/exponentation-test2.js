87985170ffbdcd15e76b7609c52cbb81cbced8e8
FaVecNer <151643268+FaVecNer@users.noreply.github.com>
2023-11-24 00:54:57 +0700

    3

--- Exponentation.script/TestUnit 1-5/exponentation-test2.js
+++ Exponentation.script/TestUnit 1-5/exponentation-test2.js
@@ -1,8 +1,8 @@
 function exponentation(n1, n2) {
     return Math.pow(n1, n2);
 }
-let otv = exponentation(2, 0)
-if (otv === 1) {
+let otv = exponentation(2, -5)
+if (otv === 0.03125) {
     console.log("Successful test" + otv)
 }
 else {
--- Exponentation.script/TestUnit 1-5/exponentation-test5.js
+++ Exponentation.script/TestUnit 1-5/exponentation-test5.js
@@ -1,3 +1,10 @@
 function exponentation(n1, n2) {
     return Math.pow(n1, n2);
 }
+let otv = exponentation(2, 0)
+if (otv === 1) {
+    console.log("Successful test" + otv)
+}
+else {
+    console.error("Test failed" + otv)
+}
