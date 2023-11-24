0ba587f61877a5d6e67591b136e286c09af4e7ba
levon125 <114795509+levon125@users.noreply.github.com>
2023-11-24 10:32:19 +0700

    Update subtraction-test-1.js
    Он теперь работает

--- test/subtraction-test-1.js
+++ test/subtraction-test-1.js
@@ -2,11 +2,12 @@
 function subtraction(num1, num2) {
     return num1 - num2;
 }
-
-let result = subtraction(7,5)
+let num1 = 7
+let num2 = 5
+let result = subtraction(num1,num2)
 if (result === 2)
 {
-    console.log("[subtraction] Unit-test completed successfully. Input params: 7(num1); 5(num2). Output results:"+ result +" (result)")
+    console.log("[subtraction] Unit-test completed successfully. Input params: num1 = " + num1 + "; num2 = "+ num2 +". Output results: result = " + result)
 } else {
-    console.error("[subtraction] Unit-test failed. Error message: (ваше сообщение). Input params: 7(num1); 5(num2). Output results: " + result +"result")
+    console.error("[subtraction] Unit-test failed. Error message: (Некорректный результат). Input params: num1 = " + num1 + "; num2 = "+ num2 +". Output results: result = " + result)
 }
