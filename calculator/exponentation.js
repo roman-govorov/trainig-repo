c7d9d46f0dcae4273a72e582467093777f199a80
FaVecNer <151643268+FaVecNer@users.noreply.github.com>
2023-11-24 22:42:25 +0700

    Add files via upload

---
+++ calculator/exponentation.js
@@ -0,0 +1,9 @@
+function exponentation(n1, n2) {
+    if (isNaN(n1) || n1 === undefined || n1 === null || n1 === Infinity || isNaN(n2) || n2 === undefined || n2 === null || n2 === Infinity) {
+        console.error("test failed, input correct number")
+    }
+    else {
+
+    }
+    return Math.pow(n1, n2);
+}
