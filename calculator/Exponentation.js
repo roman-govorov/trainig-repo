b40bea69535fc7f47a5762b05fda2913abaed496
FaVecNer <151643268+FaVecNer@users.noreply.github.com>
2023-12-26 22:57:38 +0700

    Add files via upload

--- calculator/Exponentation.js
+++ calculator/Exponentation.js
@@ -1,9 +1,9 @@
-export function exponentation(n1, n2) {
-    if (isNaN(n1) || n1 === undefined || n1 === null || n1 === Infinity || isNaN(n2) || n2 === undefined || n2 === null || n2 === Infinity) {
+export function exponentation(a, b) {
+    if (isNaN(a) || a === undefined || a === null || a === Infinity || isNaN(b) || b === undefined || b === null || b === Infinity) {
         console.error("test failed, input correct number")
     }
     else {
 
     }
-    return Math.pow(n1, n2);
+    return Math.pow(a, b);
 }
--- calculator/calc.js
+++ calculator/calc.js
@@ -1,11 +1,10 @@
+import { exponentation } from "./Exponentation.js";
 let a = ''; //first num
 let b = ''; //second num
 let sign = '';//знак операции
 let finish = false;
-
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 const action = ['-', '+', 'X', '/', '^'];
-
 //экран
 const out = document.querySelector('.calc-screen p')
 
@@ -61,7 +60,7 @@ document.querySelector('.buttons').onclick = (event) => {
         if (b === '') b = a;
         switch (sign) {
             case "^":
-                a = Math.pow(a, b);
+                a = exponentation(a, b);
                 break;
         }
         finish = true;
